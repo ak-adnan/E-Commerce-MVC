bea62904821f86929aaff574b4d7357ada507cc6
ak-adnan <adnanakanda.iths@gmail.com>
2023-09-22 23:40:32 +0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopNShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
