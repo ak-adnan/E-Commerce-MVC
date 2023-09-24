841b975d60d66c918b613104fb59784dc3732231
ak-adnan <adnanakanda.iths@gmail.com>
2023-09-25 01:07:02 +0600

    till category controller

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using ShopNShop.Constants;
 
 namespace ShopNShop.Areas.Identity.Pages.Account
 {
@@ -120,6 +121,8 @@ namespace ShopNShop.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
+                    // assing user role
+                    await _userManager.AddToRoleAsync(user,Roles.User.ToString());
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
---
+++ Data/DbSeeder.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ShopNShop.Constants;
+
+namespace ShopNShop.Data
+{
+    public class DbSeeder
+    {
+        public static async Task  SeedDefaultData (IServiceProvider service)
+        {
+            var userMgr = service.GetService<UserManager<IdentityUser>>();
+            var roleMgr = service.GetService<RoleManager<IdentityRole>>();
+
+            // adding roles to db
+            await roleMgr.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleMgr.CreateAsync(new IdentityRole(Roles.User.ToString()));
+
+            // Create admin user
+            var admin = new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true
+            };
+
+            var userInDb = await userMgr.FindByEmailAsync(admin.Email);
+            if (userInDb is null) 
+            {
+                await userMgr.CreateAsync(admin,"Admin@123");
+                await userMgr.AddToRoleAsync(admin,Roles.Admin.ToString());
+            }
+
+
+        }
+    }
+}
