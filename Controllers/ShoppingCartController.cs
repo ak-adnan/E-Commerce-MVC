2898a0881921976ee7288d3662b54d8f1b6e6b4b
ak-adnan <adnanakanda.iths@gmail.com>
2023-10-02 04:55:46 +0600

    Until add to cart details

--- Controllers/ShoppingCartController.cs
+++ Controllers/ShoppingCartController.cs
@@ -72,9 +72,67 @@ namespace ShopNShop.Controllers
             _context.SaveChanges();
 
             // Redirect to the cart view or wherever you want to go after adding to the cart
-            return RedirectToAction("Cart");
+            return RedirectToAction("CartDetails");
+        }
+        //cart view-------------------------------------
+        public IActionResult CartDetails()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account"); // Redirect to login if not authenticated
+            }
+
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            // Retrieve the cart items for the user
+            var cartItems = _context.ShoppingCarts
+                .Include(cart => cart.CartDetails)
+                .ThenInclude(cd => cd.Product) // Include the associated product
+                .FirstOrDefault(cart => cart.UserId == userId)?
+                .CartDetails;
+
+            return View(cartItems); // Pass the cart items to the view
+        }
+        // remove from cart function 
+        public IActionResult RemoveFromCart(int cartDetailsId)
+        {
+            // Find the cart item by cartDetailsId and remove it from the cart
+            var cartItem = _context.CartDetails.FirstOrDefault(item => item.CartDetailsId == cartDetailsId);
+
+            if (cartItem != null)
+            {
+                _context.CartDetails.Remove(cartItem);
+                _context.SaveChanges();
+            }
+
+            return Ok(); // Return a success status code
         }
 
 
+        [HttpGet]
+        public IActionResult GetCartItemCount()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                // If the user is not authenticated, return 0
+                return Ok(0);
+            }
+
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            ShoppingCart shoppingCart = _context.ShoppingCarts
+                .Include(cart => cart.CartDetails)
+                .FirstOrDefault(cart => cart.UserId == userId);
+
+            if (shoppingCart == null || shoppingCart.CartDetails == null)
+            {
+                // If the cart doesn't exist or is empty, return 0
+                return Ok(0);
+            }
+
+            int itemCount = shoppingCart.CartDetails.Sum(item => item.Quantity);
+
+            return Ok(itemCount);
+        }
     }
 }
